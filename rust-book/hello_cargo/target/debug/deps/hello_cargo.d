5c5613b2be8caabd700faa5e21b08a96362d8396
AllaVinner <joel.w.ottosson@gmail.com>
2022-05-29 14:59:24 +0200

    initial commit

---
+++ rust-book/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\joelw\OneDrive\Documents\Projects\Programming\rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\joelw\OneDrive\Documents\Projects\Programming\rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
