bd72ed0d9be1ea087343893e6d8ccaba517285d5
AllaVinner <joel.w.ottosson@gmail.com>
2022-12-16 14:52:16 +0100

    Added comparison project

---
+++ rust-book/comparisons/src/ordering_traits.rs
@@ -0,0 +1,49 @@
+use std::cmp::Ordering;
+
+struct A {
+    a: i32
+}
+
+
+struct B {
+    b: i32
+}
+
+// Equivilance Relations between different classes.
+
+impl PartialEq<B> for A {
+    fn eq(&self, other: &B) -> bool {
+        self.a == other.b
+    }
+}
+
+impl PartialEq<A> for B {
+    fn eq(&self, other: &A) -> bool {
+        other == self
+    }
+}
+
+
+impl PartialOrd<B> for A {
+    fn partial_cmp(&self, other: &B) -> Option<Ordering> {
+        Some(self.a.cmp(&other.b))
+    }
+}
+
+
+pub fn main() {
+    let a = A{a: 1};
+    let b = B{b: 2};
+    //println!("a == b {:?}", a == b);
+    //println!("a == b {:?}", b == a);
+    println!("a < b {:?}", a < b);
+    println!("a < b {:?}", a <= b);
+}
+
+
+
+
+
+
+
+
