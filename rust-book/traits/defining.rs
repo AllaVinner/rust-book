3da635bc6d97ed63f2710ae5c0bf625d0f466dd0
AllaVinner <joel.w.ottosson@gmail.com>
2023-01-03 22:42:24 +0100

    Adding function taking trait

--- rust-book/traits/defining.rs
+++ rust-book/traits/defining.rs
@@ -1,8 +1,14 @@
 
 
-trait TraitName {
+trait MyTrait {
 	fn fun1(&self) -> i32;
     fn fun_with_default(&self) -> i32 {
         42 as i32
     }
 }
+
+
+fn trait_fun(a: & impl MyTrait) {
+    a.fun1();
+}
+
