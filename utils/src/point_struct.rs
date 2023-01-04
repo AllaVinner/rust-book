83baf6cf03006180a6f6c7463b04295472372ec4
AllaVinner <joel.w.ottosson@gmail.com>
2023-01-04 11:06:48 +0100

    init Point in utils

---
+++ utils/src/point_struct.rs
@@ -0,0 +1,33 @@
+#[derive(Debug, Copy, Clone)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add<&Point> for &Point {
+    type Output = Point;
+
+    fn add(self, other: & Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Sub<&Point> for &Point {
+    type Output = Point;
+
+    fn sub(self, other: & Point) -> Point {
+        Point {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        return self.x == other.x && self.y == other.y;
+    }
+}
