e676f9a6ceb9daf7a2a3592642dd4f23982e067f
AllaVinner <joel.w.ottosson@gmail.com>
2023-01-04 10:04:13 +0100

    Including Trait bounds in Traits folder

--- rust-book/traits/defining.rs
+++ rust-book/traits/defining.rs
@@ -1,14 +1,26 @@
 
 
+// Defining Trait
 trait MyTrait {
+    // Declare Trait function
 	fn fun1(&self) -> i32;
+    // Initiate default function (Can be overwritten)
     fn fun_with_default(&self) -> i32 {
         42 as i32
     }
 }
 
-
+// Traits as parameters
 fn trait_fun(a: & impl MyTrait) {
     a.fun1();
 }
 
+// Trait bound
+fn bound_fun<T: MyTrait>(item: &T) {
+    item.fun_with_default();
+}
+
+// Bound with same type
+fn same_type_fun<T: MyTrait>(a: &T, b: &T){
+    
+}
