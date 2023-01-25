d8dd65d10e4688104c9cf7128b6f7bb35a5d430a
AllaVinner <joel.w.ottosson@gmail.com>
2023-01-25 22:12:31 +0100

    Working image example for utils

--- utils/src/image_handler.rs
+++ utils/src/image_handler.rs
@@ -1,11 +1,12 @@
 use image::io::Reader as ImageReader;
+use image;
 
 fn type_of<T>(_:&T) -> &'static str {
     std::any::type_name::<T>()
 }
 
 pub fn main() {
-    let img = ImageReader::open("blocks.jpg")?.decode()?;
+    let img = image::open("blocks.jpg").expect("File not found!");
     //let img2 = ImageReader::new(Cursor::new(bytes)).with_guessed_format()?.decode()?;
     println!("{:?}", type_of(&img));
     //println!("{:?}", type_of::type_of(&img2));
