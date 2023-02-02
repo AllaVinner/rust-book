f5d95b23635996918166efc089c5014b9f71a519
AllaVinner <joel.w.ottosson@gmail.com>
2023-02-02 08:33:53 +0100

    Added capability for Optional Option in CLI

--- utils/src/cli_input.rs
+++ utils/src/cli_input.rs
@@ -1,19 +1,26 @@
 use clap::{Parser, ValueEnum};
+use clap;
 use std::string::String;
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, 
+    about="THis is a great command", 
+    long_about = "Actually This is the greatest that ever existed")]
 struct Args {
     #[arg()]
     basic_arg: String,
 
    #[arg(short, long, default_value_t = -0.5)]
    basic_option: f32,
+   
+   #[arg(short, long, default_value = None)]
+   optional_option: Option<f32>,
 
    #[arg(short, long, default_value_t = String::from("Default Input"))]
    string: String,
 
-   #[arg(value_enum, short='a', long, default_value_t = Category::A)]
+   #[arg(value_enum, short='a', long, default_value_t = Category::A, 
+   long_help="This command is a bit more complex than the others")]
    category: Category,
 }
 
