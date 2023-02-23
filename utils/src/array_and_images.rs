fb173d68aa588c59d4eaa72038d5aa64d5f2e193
AllaVinner <joel.w.ottosson@gmail.com>
2023-02-23 22:55:18 +0100

    Working blur with CLI

--- utils/src/array_and_images.rs
+++ utils/src/array_and_images.rs
@@ -1,5 +1,5 @@
 use std::ops::{Add, Div};
-
+use clap::{Parser, ValueEnum};
 use image::{GenericImage, GenericImageView, ImageBuffer, open, image_dimensions, Rgb};
 use ndarray::{Array3, Dim, ArrayView3, IntoDimension, Array};
 use num_traits::{FromPrimitive, Zero, zero};
@@ -15,14 +15,30 @@ where
         .unwrap()
 }
 
+#[derive(Parser, Debug)]
+#[command(author, version, 
+    about="THis is a great command", 
+    long_about = "Actually This is the greatest that ever existed")]
+struct Args {
+   #[arg(short, long, default_value_t = String::from("fractal.png"))]
+   in_path: String,
+
+   #[arg(short, long, default_value_t = String::from("fractal_out.png"))]
+   out_path: String,
+}
 
 pub fn main() {
-    let (height, width) = image_dimensions("fractal.png").unwrap();
-    let image = Array3::from_shape_vec(Dim([height as usize, width as usize,3]), open("fractal.png").unwrap().into_bytes()).unwrap();
+    let args: Args = Args::parse();
+    let (height, width) = image_dimensions(&args.in_path).unwrap();
+    let image = Array3::from_shape_vec(Dim([height as usize, width as usize,3]), open(args.in_path).unwrap().into_bytes()).unwrap();
 
+    println!("Before");
     let blurred = blur(image.view(), [10,10,1]);
-    let bimage:ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_raw(width, height, blurred.into_raw_vec()).unwrap();
-    bimage.save("fractal_blurr.png").unwrap();
+    println!("After");
+    println!("{:?}", blurred.shape());
+    println!("{:?}", image.shape());
+    let bimage:ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_raw(blurred.shape()[0] as u32, blurred.shape()[1] as u32, blurred.into_raw_vec()).unwrap();
+    bimage.save(args.out_path).unwrap();
 
 }
 
