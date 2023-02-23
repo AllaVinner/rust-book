fbca68fbf560c76a08754525d6dc8795c9f0b47d
AllaVinner <joel.w.ottosson@gmail.com>
2023-02-23 20:16:00 +0100

    Reading image and converting to ndarray

--- utils/src/array_and_images.rs
+++ utils/src/array_and_images.rs
@@ -1,10 +1,12 @@
 use image::{GenericImage, GenericImageView, ImageBuffer, open, image_dimensions};
-use ndarray::{Array3};
+use ndarray::{Array3, Dim};
 
 pub fn main() {
-    let image = open("fractal.png").unwrap();
-    let (height, width) = image_dimensions("fractal.png").unwrap(); 
-    println!("{:?}", height);
+    let (height, width) = image_dimensions("fractal.png").unwrap();
+    let image = Array3::from_shape_vec(Dim([height as usize, width as usize,3]), open("fractal.png").unwrap().into_bytes()).unwrap();
+    
+     
+    println!("{:?}", image.dim());
 }
 
 
