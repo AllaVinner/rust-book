f900adcf23bb62516ea83674a50456da5d75db6d
AllaVinner <joel.w.ottosson@gmail.com>
2023-02-26 19:59:08 +0100

    Closure in generics

--- rust-book/closures/src/main.rs
+++ rust-book/closures/src/main.rs
@@ -7,6 +7,18 @@ fn basic_use() {
     dbg!(a.iter().map(|v| v+10));
 }
 
+impl<T> Option<T> {
+    pub fn unwrap_or_else<F>(self, f: F) -> T
+    where
+        F: FnOnce() -> T
+    {
+        match self {
+            Some(x) => x,
+            None => f(),
+        }
+    }
+}
+
 fn main() {
     basic_use();
 }
