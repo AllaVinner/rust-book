63495a47f16b1ec4027729af3ebee0bb2cd8341b
AllaVinner <joel.w.ottosson@gmail.com>
2023-02-27 20:58:19 +0100

    Consuming and adaptor iterators examples

--- rust-book/iterators/src/main.rs
+++ rust-book/iterators/src/main.rs
@@ -1,3 +1,22 @@
+fn consuming_adaptor() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.sum();
+
+    assert_eq!(total, 6);
+}
+
+fn iterator_adaptor() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.map(|x| x+1);
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
