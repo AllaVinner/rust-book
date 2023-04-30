03f9bd0d3248ecb4b7bfa1ceee7774e0754e58bd
AllaVinner <joel.w.ottosson@gmail.com>
2023-04-30 09:31:56 +0200

    Fitrst arrow script

--- rust-book/arrow/src/main.rs
+++ rust-book/arrow/src/main.rs
@@ -1,3 +1,13 @@
+use arrow::*;
+use arrow::array::Array;
+
 fn main() {
-    println!("Hello, world!");
+    let array = array::Int32Array::from(vec![Some(1), None, Some(3)]);
+    assert_eq!(array.len(), 3);
+    assert_eq!(array.value(0), 1);
+    assert_eq!(array.is_null(1), true);
+    
+    let collected: Vec<_> = array.iter().collect();
+    assert_eq!(collected, vec![Some(1), None, Some(3)]);
+    assert_eq!(array.values(), &[1, 0, 3])
 }
